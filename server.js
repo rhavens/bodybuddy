944639ff4a738cbcc08cfb965cfe6e5a3f753210
Ryan Havens <ryan.d.havens@gmail.com>
2014-10-29 16:14:06 -0400

    Added example server code

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var url = require("url");
+
+http.createServer(function(request, response) {
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " received.");
+	var query = url.parse(request.url).query;
+	console.log("Query " + query + " received.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	if (pathname == "/pikachu") {
+		response.write("You've won the game!");
+	}
+	else {
+		response.write("<html><h1>Hello World</h1></html>");
+	}
+	response.end();
+    }).listen(8888);
