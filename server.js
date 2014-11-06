131f52e050a613de41de5f19927f757a4f125874
Ryan Havens <ryan.d.havens@gmail.com>
2014-11-06 16:27:56 -0500

    Server.js rewrite

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var __static = path.resolve('../static');
-var __views = path.resolve('../views');
+var __static = path.resolve('static');
+var __views = path.resolve('views');
 
 app.use('/img', express.static(__static + '/img'));
 app.use('/css', express.static(__static + '/css'));
+app.use('/js', express.static(__static + '/js'));
 
 app.get('/', function(req, res){
     res.sendFile(__views + '/index.html'); 
