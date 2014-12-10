d2ba1edde3ff34e385625200eb089dfd0589d297
Ryan Havens <ryan.d.havens@gmail.com>
2014-12-09 21:10:18 -0500

    Pizza

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ function getProfile(identifier) {
               pizza = "pizza";
           }
           else {
-              pizza = JSON.parse(JSON.stringify(cursor[0]));
+              return "fish";
           }
         });
     });
@@ -176,10 +176,12 @@ app.get('/profile', ensureAuthenticated, function(req, res){
     var index = "<!DOCTYPE HTML><html><head><title>What Did You Feed Me?</title></head><body><h1>What Did You Feed Me?</h1>";
     var profile = getProfile(identifier);
     index += JSON.stringify(profile);
-    var history = getHistory(identifier);/*{'account':1,'history':[{'time':0,'avg':150},{'time':1,'avg':200}]};*/
+    console.log(
+    //var history = getHistory(identifier);/*{'account':1,'history':[{'time':0,'avg':150},{'time':1,'avg':200}]};*/
     // debugging
     index += identifier;
-    index += history;
+ //   console.log(history);
+  //  index += history;
     index += "</body></html>";
     res.send(index);
  /*   if (!profile) {
